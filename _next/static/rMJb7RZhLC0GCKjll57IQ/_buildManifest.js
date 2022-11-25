b09bd2f27776db265622cb98024771c209f3fdad
FelixSelter <felixselterdeveloping@gmail.com>
2022-11-25 23:23:43 +0100

    Updates

--- _next/static/rMJb7RZhLC0GCKjll57IQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a4046a426d3d0028.css","static/chunks/pages/index-e7f3e8e33a1f705c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
